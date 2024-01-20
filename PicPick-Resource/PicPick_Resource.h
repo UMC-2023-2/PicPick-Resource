f2e43b0957bdcc2bf0d714ccb8207d2ff8afc459
Jaeuk Jeong <jaeuk@jeuke.com>
2024-01-20 10:50:21 +0900

    Initial Commit

---
+++ PicPick-Resource/PicPick_Resource.h
@@ -0,0 +1,18 @@
+//
+//  PicPick_Resource.h
+//  PicPick-Resource
+//
+//  Created by Jaeuk on 1/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PicPick_Resource.
+FOUNDATION_EXPORT double PicPick_ResourceVersionNumber;
+
+//! Project version string for PicPick_Resource.
+FOUNDATION_EXPORT const unsigned char PicPick_ResourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PicPick_Resource/PublicHeader.h>
+
+
