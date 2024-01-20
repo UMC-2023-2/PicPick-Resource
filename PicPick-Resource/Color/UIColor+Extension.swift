ced0fb7d5f20b9a7fa2ee8119aa5fb9ddd86f594
Jaeuk Jeong <jaeuk@jeuke.com>
2024-01-20 11:16:22 +0900

    Add Colors

---
+++ PicPick-Resource/Color/UIColor+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  UIColor+Extension.swift
+//  PicPick-Resource
+//
+//  Created by Jaeuk on 1/20/24.
+//
+
+import UIKit
+
+public extension R {
+    enum Color { }
+}
+
+public extension R.Color {
+    static var gray300: UIColor { .load(name: "Gray/300") }
+    static var gray400: UIColor { .load(name: "Gray/400") }
+    static var gray500: UIColor { .load(name: "Gray/500") }
+    static var gray600: UIColor { .load(name: "Gray/600") }
+    static var gray700: UIColor { .load(name: "Gray/700") }
+    static var gray800: UIColor { .load(name: "Gray/800") }
+    static var gray900: UIColor { .load(name: "Gray/900") }
+    
+    static var pointLight: UIColor { .load(name: "Point/Light") }
+    static var pointNormal: UIColor { .load(name: "Point/Normal") }
+    
+    static var systemBlack: UIColor { .load(name: "System/Black") }
+    static var systemWhite: UIColor { .load(name: "System/White") }
+    static var systemRed: UIColor { .load(name: "System/Red") }
+}
+
+extension UIColor {
+    static func load(name: String) -> UIColor {
+        guard let color = UIColor(named: name, in: R.bundle, compatibleWith: nil) else {
+            assert(false, "\(name) 컬러 로드 실패")
+            return UIColor()
+        }
+        return color
+    }
+}
---
+++ PicPick-Resource/R.swift
@@ -0,0 +1,12 @@
+//
+//  R.swift
+//  PicPick-Resource
+//
+//  Created by Jaeuk on 1/20/24.
+//
+
+import Foundation
+
+public class R {
+    static let bundle = Bundle(for: R.self)
+}
